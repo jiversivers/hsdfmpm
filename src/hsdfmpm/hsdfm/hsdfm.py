bc0ac23df52824fc178286098fcf8762cac519d4
jiversivers <jdivers@uark.edu>
2025-04-23 09:35:41 -0500

    Refactor hyperspectral image classes and normalization logic
    Simplify class definitions, enhance normalization methods, and add scalar support. Introduce `MergedHyperspectralImage` enhancements with arithmetic methods and improve attribute delegation for streamlined processing.

--- src/hsdfmpm/hsdfm/hsdfm.py
+++ src/hsdfmpm/hsdfm/hsdfm.py
@@ -1,12 +1,10 @@
-from functools import cache, cached_property
-from pathlib import Path
-from typing import Union, Optional
+from functools import cache
+from typing import Optional
 
-import numpy as np
-from pydantic import model_validator, field_validator, BaseModel, computed_field
+from pydantic import model_validator, BaseModel, SkipValidation
 
 from .utils import *
-from ..utils import ImageData, read_hyperstack, ensure_path
+from ..utils import ImageData, read_hyperstack, add_arithmetic_methods
 
 
 class HyperspectralImage(ImageData):
@@ -19,6 +17,9 @@ class HyperspectralImage(ImageData):
     model: ModelType = 'monte_carlo'
     metadata_ext: str = 'metadata.json'
     image_ext: str = '.tiff'
+    standard: Optional[SkipValidation[Union['MergedHyperspectralImage', 'HyperspectralImage']]] = None
+    background: Optional[SkipValidation[Union['MergedHyperspectralImage', 'HyperspectralImage']]] = None
+    scalar: Union[float, np.ndarray[float]] = 1
 
     @model_validator(mode='after')
     def load_data(self):
@@ -28,71 +29,61 @@ class HyperspectralImage(ImageData):
         self.metadata = read_metadata_json(metadata_path)
 
         # Load hyperstack
-        self._hyperstack = read_hyperstack(img_dir=self.image_path, ext=self.image_ext)
-
-        # Store raw instance for back reference after mutating
-        self.raw = self._hyperstack.copy()
+        hyperstack = read_hyperstack(img_dir=self.image_path, ext=self.image_ext)
+        self._hyperstack = hyperstack if self.scalar is None else hyperstack / self.scalar
+        self._active = self.hyperstack
         return self
 
     def normalize_integration_time(self):
-        self._hyperstack = normalize_integration_time(self, self.metadata['ExpTime'])
-        self._int_normed = True
+        self._active = normalize_integration_time(self, self.metadata['ExpTime'])
 
     def normalize_to_standard(self):
-        if not (hasattr(self, 'standard') and hasattr(self.standard, 'bg')):
+        if self.standard is None or self.background is None:
             raise ValueError(f'"{self.image_path}" is missing "standard" and/or "background" attributes.\n'
                              f'Set them and try again.')
-        self._hyperstack = normalize_to_standard(self, self.standard, self.bg)
-        self._standard_normed = True
+        self._active = normalize_to_standard(self, self.standard, self.background)
 
     def normalize(self):
-        if not self._int_normed:
-            self.normalize_integration_time()
-        if not self._standard_normed:
+        self.normalize_integration_time()
+        if self.standard is not None and self.background is not None:
             self.normalize_to_standard()
-        return self._hyperstack
 
     @cache
-    def fit(self):
+    def fit(self, model: ModelType = 'monte_carlo', **kwargs):
         pass
 
-    # def k_cluster
+    def k_cluster(self, k: Union[int, list[int]] = 2):
+        if isinstance(k, int):
+            k = [k]
+        pass
 
-class MergedHyperspectralImage(ImageData):
+@add_arithmetic_methods
+class MergedHyperspectralImage(BaseModel):
     """
     This class is effectively a list of HyperspectralImages with easy, built-in iteration over that list, and
     implicit merging when accessed, so it can be easily used as a HyperspectralImage object for processing.
     """
-    image_path: list[Union[str, Path]]
+    image_paths: list[Union[str, Path]]
+    image_ext: str = '.tiff'
+    channels: Optional[Union[int, list[int]]] = None
+    metadata_path: Optional[Union[str, Path]] = None
     metadata_ext: str = 'metadata.json'
     listed_hyperstack: Optional[list[HyperspectralImage]] = None
+    scalar: float = 1
 
     @model_validator(mode='after')
     def load_data(self):
         self.listed_hyperstack = []
-        for image_path in self.image_path:
+        for image_path in self.image_paths:
             kwargs = dict(image_path=image_path,
                           image_ext=self.image_ext,
                           metadata_ext=self.metadata_ext,
-                          channels=self.channels)
+                          channels=self.channels,
+                          scalar=self.scalar)
             if self.metadata_path is not None:
                 kwargs['metadata_path'] = self.metadata_path
             self.listed_hyperstack.append(HyperspectralImage(**kwargs))
-
         return self
-
-    @computed_field
-    @property
-    def hyperstack(self) -> np.ndarray:
-        if self._hyperstack is None:
-            self._hyperstack = np.mean([image.hyperstack for image in self.listed_hyperstack], axis=0)
-        return self._hyperstack
-
-    @computed_field
-    @cached_property
-    def raw(self) -> np.ndarray:
-        # Get initial merged hyperstack for back reference after mutation
-        return np.mean([image.hyperstack for image in self.listed_hyperstack], axis=0)
 
     def __getattr__(self, attr):
         """Forward method calls to and get attributes from each instance of HyperspectralImage in the list."""
@@ -108,7 +99,14 @@ class MergedHyperspectralImage(ImageData):
 
             return delegated
         else:
-            return [getattr(member, attr) for member in self.listed_hyperstack]
+            out = [getattr(member, attr) for member in self.listed_hyperstack]
+            try:
+                return np.nanmean(out, axis=0)
+            except TypeError:
+                return out
+
+    def __array__(self):
+        return self._active
 
     def __getitem__(self, item):
         return self.listed_hyperstack[item]
