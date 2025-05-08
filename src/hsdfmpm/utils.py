d984754f62446a1835854d0f617747a0f6d84ffb
jiversivers <jdivers@uark.edu>
2025-05-08 16:37:04 -0500

    black formatted and removed pbar

--- src/hsdfmpm/utils.py
+++ src/hsdfmpm/utils.py
@@ -14,10 +14,11 @@ from matplotlib.colors import Colormap, Normalize
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, Colormap, LinearSegmentedColormap
 from pydantic import BaseModel, Field, computed_field, AfterValidator, model_validator
-from tqdm.contrib import itertools
+import itertools
 
 DATA_PATH = Path.home() / ".hsdfmpm"
 DATA_PATH.mkdir(exist_ok=True, parents=True)
+
 
 # Path helpers
 def ensure_path(path_like: Union[str, Path]) -> Path:
@@ -28,16 +29,22 @@ def ensure_path(path_like: Union[str, Path]) -> Path:
             return Path(urlparse(path_like).path)
         return Path(path_like)
     if isinstance(path_like, NoneType):
-        warnings.warn("'ensure_path' got NoneType, but expected str or Path object.", UserWarning, stacklevel=2)
+        warnings.warn(
+            "'ensure_path' got NoneType, but expected str or Path object.",
+            UserWarning,
+            stacklevel=2,
+        )
         return None
     raise TypeError(f"Cannot convert {type(path_like)} to Path")
+
 
 def is_file(file_path: str) -> Path:
     file_path = ensure_path(file_path)
     if not file_path.is_file():
         raise FileNotFoundError(file_path)
-    print('File passed')
+    print("File passed")
     return file_path
+
 
 def is_dir(file_path: str) -> Path:
     file_path = ensure_path(file_path)
@@ -45,10 +52,12 @@ def is_dir(file_path: str) -> Path:
         raise NotADirectoryError(file_path)
     return file_path
 
+
 # Helper for dtype conversion
 def iterable_array(x: Any) -> Iterable[Any]:
     x = [x] if not isinstance(x, Iterable) else x
     return np.array(x)
+
 
 # Helper function to handle reshaping input images
 def vectorize_img(img: np.ndarray, include_location: bool = False) -> np.ndarray:
@@ -59,14 +68,18 @@ def vectorize_img(img: np.ndarray, include_location: bool = False) -> np.ndarray
         shape = img.shape[1:]
         X = img.reshape(img.shape[0], -1).T
     if include_location:
-        x, y = np.meshgrid(np.linspace(-0.5, 0.5, shape[0]) * shape[0], np.linspace(-0.5, 0.5, shape[1]) * shape[1])
+        x, y = np.meshgrid(
+            np.linspace(-0.5, 0.5, shape[0]) * shape[0],
+            np.linspace(-0.5, 0.5, shape[1]) * shape[1],
+        )
         X = np.column_stack([x.ravel(), y.ravel(), X])
     return X
 
+
 # Image reader
-def read_hyperstack(img_dir: str, ext: str = '.tif') -> np.ndarray[float]:
+def read_hyperstack(img_dir: str, ext: str = ".tif") -> np.ndarray[float]:
     hs = []
-    for img_path in Path(img_dir).glob(f'*{ext}'):
+    for img_path in Path(img_dir).glob(f"*{ext}"):
         hs.append(cv2.imread(img_path, cv2.IMREAD_UNCHANGED))
     if not img_path:
         raise FileNotFoundError(f"No image files found in {img_dir}.")
@@ -75,17 +88,25 @@ def read_hyperstack(img_dir: str, ext: str = '.tif') -> np.ndarray[float]:
         hs = np.expand_dims(hs, axis=0)
     return hs
 
-def apply_kernel_bank(src: np.ndarray[float], bank: np.ndarray[float]) -> np.ndarray[float]:
-    return np.nanmax([
-            cv2.filter2D(src, -1, k) for (k,) in itertools.product(bank)
-        ], axis=0)
+
+def apply_kernel_bank(
+    src: np.ndarray[float], bank: np.ndarray[float]
+) -> np.ndarray[float]:
+    return np.nanmax(
+        [cv2.filter2D(src, -1, k) for (k,) in itertools.product(bank)], axis=0
+    )
 
 
-def truncate_colormap(cmap: Optional[Union[Colormap, np.ndarray[float], str]] = None,
-                      cmin: float = 0, cmax: float = 1, n: int = 100):
+def truncate_colormap(
+    cmap: Optional[Union[Colormap, np.ndarray[float], str]] = None,
+    cmin: float = 0,
+    cmax: float = 1,
+    n: int = 100,
+):
     cmap = get_cmap(cmap)
     new_colors = cmap(np.linspace(cmin, cmax, n))
-    return LinearSegmentedColormap.from_list('truncated_cmap', new_colors, N=n)
+    return LinearSegmentedColormap.from_list("truncated_cmap", new_colors, N=n)
+
 
 def get_cmap(cmap: Optional[Union[Colormap, np.ndarray[float], str]] = None):
     if not isinstance(cmap, Colormap):
@@ -94,7 +115,7 @@ def get_cmap(cmap: Optional[Union[Colormap, np.ndarray[float], str]] = None):
         elif isinstance(cmap, str):
             cmap = cm.get_cmap(cmap)
         elif cmap is None:
-            cmap = cm.get_cmap('jet')
+            cmap = cm.get_cmap("jet")
         else:
             raise TypeError(
                 f"cmap must be either a Colormap, str, or None to default to 'jet', but got type {type(cmap)}"
@@ -102,21 +123,23 @@ def get_cmap(cmap: Optional[Union[Colormap, np.ndarray[float], str]] = None):
     return cmap
 
 
-def colorize(var_map: np.ndarray[float],
-             intensity: Optional[np.ndarray[float]] = None,
-             cmap: Optional[Union[Colormap, np.ndarray]] = None,
-             cmin: float = 0.0,
-             cmax: float = 1.0,
-             n: int = 100,
-             intmin: float = 0.1,
-             intmax: float = 0.95) -> tuple[np.ndarray[float], Colormap]:
+def colorize(
+    var_map: np.ndarray[float],
+    intensity: Optional[np.ndarray[float]] = None,
+    cmap: Optional[Union[Colormap, np.ndarray]] = None,
+    cmin: float = 0.0,
+    cmax: float = 1.0,
+    n: int = 100,
+    intmin: float = 0.1,
+    intmax: float = 0.95,
+) -> tuple[np.ndarray[float], Colormap]:
 
     if intensity is None:
         intensity = np.ones_like(var_map)
 
     if cmap is None:
         # Default cmap if none is input
-        cmap = truncate_colormap('jet', 0.13, 0.88, n)
+        cmap = truncate_colormap("jet", 0.13, 0.88, n)
     else:
         # Validate input
         cmap = get_cmap(cmap)
@@ -134,22 +157,23 @@ def colorize(var_map: np.ndarray[float],
 
     return colorized * intensity[..., np.newaxis], cmap
 
+
 # Define the class decorator
 def add_arithmetic_methods(cls):
     # Mapping of dunder methods to their respective operator functions
     methods = {
-        '__add__': operator.add,
-        '__sub__': operator.sub,
-        '__mul__': operator.mul,
-        '__truediv__': operator.truediv,
-        '__floordiv__': operator.floordiv,
-        '__mod__': operator.mod,
-        '__pow__': operator.pow,
-        '__lt__': operator.lt,
-        '__le__': operator.le,
-        '__gt__': operator.gt,
-        '__ge__': operator.ge,
-        '__eq__': operator.eq,
+        "__add__": operator.add,
+        "__sub__": operator.sub,
+        "__mul__": operator.mul,
+        "__truediv__": operator.truediv,
+        "__floordiv__": operator.floordiv,
+        "__mod__": operator.mod,
+        "__pow__": operator.pow,
+        "__lt__": operator.lt,
+        "__le__": operator.le,
+        "__gt__": operator.gt,
+        "__ge__": operator.ge,
+        "__eq__": operator.eq,
     }
 
     # Dynamically add methods to the class
@@ -166,6 +190,7 @@ def add_arithmetic_methods(cls):
 
     return cls
 
+
 @overload
 def channel_check(channels: NoneType) -> NoneType: ...
 @overload
@@ -173,9 +198,13 @@ def channel_check(channels: tuple[int]) -> tuple[int]: ...
 @overload
 def channel_check(channels: Union[int, list[int]]) -> list[int]: ...
 
-def channel_check(channels: Union[int, list[int], tuple[int]]) -> Union[tuple[int], list[int]]:
+
+def channel_check(
+    channels: Union[int, list[int], tuple[int]],
+) -> Union[tuple[int], list[int]]:
     channels = [channels] if isinstance(channels, int) else channels
     return channels
+
 
 @add_arithmetic_methods
 class ImageData(BaseModel):
@@ -185,17 +214,20 @@ class ImageData(BaseModel):
 
     :param image_path: str; The directory path of the image cube and metadata file.
     """
+
     image_path: Annotated[Union[str, Path], AfterValidator(is_dir)]
-    image_ext: str = '.tiff'
+    image_ext: str = ".tiff"
     metadata_ext: str
     metadata_path: Annotated[Optional[Union[str, Path]], AfterValidator(is_file)] = None
     metadata: Optional[dict] = None
-    channels: Annotated[Optional[Union[int, list[int], tuple[int]]], AfterValidator(channel_check)] = None
+    channels: Annotated[
+        Optional[Union[int, list[int], tuple[int]]], AfterValidator(channel_check)
+    ] = None
     _hyperstack: Optional[np.ndarray] = None
     _active: Optional[np.ndarray] = None
 
     class Config:
-        extra = 'allow'
+        extra = "allow"
         arbitrary_types_allowed = True
 
     @computed_field
@@ -204,9 +236,13 @@ class ImageData(BaseModel):
         if self._hyperstack is None:
             self._hyperstack = read_hyperstack(self.image_path, self.image_ext)
         # Select out channels
-        hyperstack = self._hyperstack.copy()  # So channels can be selected after initialization
+        hyperstack = (
+            self._hyperstack.copy()
+        )  # So channels can be selected after initialization
         if self.channels is not None:
-            hyperstack = np.stack([self._hyperstack[ch] for ch in self.channels], axis=0)
+            hyperstack = np.stack(
+                [self._hyperstack[ch] for ch in self.channels], axis=0
+            )
         return hyperstack
 
     @computed_field
@@ -246,13 +282,11 @@ class ImageData(BaseModel):
         w_binned = w // bin_factor
 
         # Crop to make divisible
-        cube_cropped = self._active[:, :h_binned * bin_factor, :w_binned * bin_factor]
+        cube_cropped = self._active[:, : h_binned * bin_factor, : w_binned * bin_factor]
 
         # Reshape and bin
         cube_reshaped = cube_cropped.reshape(
-            bands,
-            h_binned, bin_factor,
-            w_binned, bin_factor
+            bands, h_binned, bin_factor, w_binned, bin_factor
         )
 
         # Average over binning axes
@@ -267,9 +301,10 @@ class ImageData(BaseModel):
     def apply_mask(self, mask):
         self._active[:, ~mask.astype(bool)] = np.nan
 
-    def write(self, filename: str = 'processed.tiff', **kwargs):
+    def write(self, filename: str = "processed.tiff", **kwargs):
         filename = ensure_path(filename)
         cv2.imwrite(str(filename), self._active, **kwargs)
+
 
 class SerializableModel(BaseModel):
     __version__ = "0.0.1"
@@ -278,7 +313,7 @@ class SerializableModel(BaseModel):
     def save_json(self, path: Union[str, Path], **kwargs):
         path = ensure_path(path)
         data = self.model_dump()
-        data['model_version'] = self.__version__
+        data["model_version"] = self.__version__
         path.write_text(json.dumps(data, **kwargs))
 
     @classmethod
@@ -305,6 +340,5 @@ class SerializableModel(BaseModel):
     def _report_version(cls, version):
         if version != cls.__version__:
             warnings.warn(
-                f"Loaded version {version} of {cls.__class__.__name__}",
-                stacklevel=2
+                f"Loaded version {version} of {cls.__class__.__name__}", stacklevel=2
             )
