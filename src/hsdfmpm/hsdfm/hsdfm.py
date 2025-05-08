6a676a295b8350b00efdc73ed96905c4f908f48f
jiversivers <jdivers@uark.edu>
2025-05-08 16:38:02 -0500

    black formatted and added heavy fit method refactor (now relies heavily on brother module and parallelizes in multiprocessors)

--- src/hsdfmpm/hsdfm/hsdfm.py
+++ src/hsdfmpm/hsdfm/hsdfm.py
@@ -7,6 +7,7 @@ import scipy.optimize as opt
 
 from .utils import *
 from ..utils import ImageData, read_hyperstack, add_arithmetic_methods
+from .fit import fit_volume
 
 
 class HyperspectralImage(ImageData):
@@ -16,16 +17,21 @@ class HyperspectralImage(ImageData):
 
     :param image_path: str; The directory path of the HSDFM image cube and JSON metadata file.
     """
-    metadata_ext: str = 'metadata.json'
-    image_ext: str = '.tiff'
-    standard: Optional[SkipValidation[Union['MergedHyperspectralImage', 'HyperspectralImage']]] = None
-    background: Optional[SkipValidation[Union['MergedHyperspectralImage', 'HyperspectralImage']]] = None
+
+    metadata_ext: str = "metadata.json"
+    image_ext: str = ".tiff"
+    standard: Optional[
+        SkipValidation[Union["MergedHyperspectralImage", "HyperspectralImage"]]
+    ] = None
+    background: Optional[
+        SkipValidation[Union["MergedHyperspectralImage", "HyperspectralImage"]]
+    ] = None
     wavelengths: Optional[list[float]] = None
     scalar: Union[float, np.ndarray[float]] = 1
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def load_data(self):
-        metadata_path = list(self.image_path.glob(f'*{self.metadata_ext}'))[0]
+        metadata_path = list(self.image_path.glob(f"*{self.metadata_ext}"))[0]
 
         # Load image metadata
         self.metadata = read_metadata_json(metadata_path).copy()
@@ -41,22 +47,27 @@ class HyperspectralImage(ImageData):
 
         # Apply wavelength selection to image and metadata
         if self.wavelengths is not None:
-            mask = np.isin(self.metadata['Wavelength'], self.wavelengths)
+            mask = np.isin(self.metadata["Wavelength"], self.wavelengths)
             self._hyperstack = self._hyperstack[mask]
             for key in self.metadata.keys():
-                self.metadata[key] = [self.metadata[key][i] for i, is_in_mask in enumerate(mask) if is_in_mask]
+                self.metadata[key] = [
+                    self.metadata[key][i]
+                    for i, is_in_mask in enumerate(mask)
+                    if is_in_mask
+                ]
         self._hyperstack /= self.scalar if scale_after_masking else 1
         self._active = self.hyperstack
         return self
 
-
     def normalize_integration_time(self):
-        self._active = normalize_integration_time(self, self.metadata['ExpTime'])
+        self._active = normalize_integration_time(self, self.metadata["ExpTime"])
 
     def normalize_to_standard(self):
         if self.standard is None or self.background is None:
-            raise ValueError(f'"{self.image_path}" is missing "standard" and/or "background" attributes.\n'
-                             f'Set them and try again.')
+            raise ValueError(
+                f'"{self.image_path}" is missing "standard" and/or "background" attributes.\n'
+                f"Set them and try again."
+            )
         self._active = normalize_to_standard(self, self.standard, self.background)
 
     def normalize(self):
@@ -66,44 +77,26 @@ class HyperspectralImage(ImageData):
 
     def get(self, wavelength: float) -> np.ndarray[float]:
         """Method to streamline selection of specific wavelengths from the image stack."""
-        for wl in self.metadata['Wavelength']:
+        for wl in self.metadata["Wavelength"]:
             if wl == wavelength:
-                return self[self.metadata['Wavelength'].index(wl)]
+                return self[self.metadata["Wavelength"].index(wl)]
 
-    @cache
-    def fit(self, model: Callable[[float, float, ...], np.ndarray[float]],
-            guess: list[float], bounds: list[tuple[float, float]]) -> tuple[np.ndarray[float], np.ndarray[bool]]:
+    def fit(
+        self,
+        model: Callable[[float, float, ...], np.ndarray[float]],
+        x0: np.ndarray[float],
+        **kwargs) -> tuple[np.ndarray[float], np.ndarray[float]]:
 
-        # Get image shape
-        image_shape = self.shape
+        # Fit in parallel
+        param_image, score = fit_volume(
+            volume=self.image,
+            model=model,
+            x0=x0,
+            **kwargs
+        )
 
-        # Mask nans
-        nan_mask = np.logical_or(np.isnan(self), np.isinf(self))
+        return param_image, score
 
-        # Create 0 output array
-        out_image = np.zeros((len(bounds),) + image_shape[1:])
-        params = guess
-        for i, j in itertools.product(range(image_shape[1]), range(image_shape[2])):
-            # Get current voxel
-            r = self[:, i, j]
-            mask = nan_mask[:, i, j]
-
-            # Remove nan values
-            if np.any(mask):
-                r = r[~mask]
-                wavelengths = self.wavelengths[~mask]
-            try:
-                params, _ = opt.curve_fit(model, wavelengths, r, p0=params, bounds=bounds)
-            except RuntimeError:
-                params = [np.nan] * 4
-
-            # Store fitted out
-            out_image[:, i, j] = params
-
-            # Update guess to params for faster convergence
-            params = guess if np.isnan(params).any() else params
-
-        return out_image, nan_mask
 
 @add_arithmetic_methods
 class MergedHyperspectralImage(BaseModel):
@@ -111,27 +104,30 @@ class MergedHyperspectralImage(BaseModel):
     This class is effectively a list of HyperspectralImages with easy, built-in iteration over that list, and
     implicit merging when accessed, so it can be easily used as a HyperspectralImage object for processing.
     """
+
     image_paths: list[Union[str, Path]]
-    image_ext: str = '.tiff'
+    image_ext: str = ".tiff"
     channels: Optional[Union[int, list[int]]] = None
     metadata_path: Optional[Union[str, Path]] = None
-    metadata_ext: str = 'metadata.json'
+    metadata_ext: str = "metadata.json"
     listed_hyperstack: Optional[list[HyperspectralImage]] = None
     wavelengths: Optional[list[float]] = None
     scalar: float = 1
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def load_data(self):
         self.listed_hyperstack = []
         for image_path in self.image_paths:
-            kwargs = dict(image_path=image_path,
-                          image_ext=self.image_ext,
-                          metadata_ext=self.metadata_ext,
-                          channels=self.channels,
-                          scalar=self.scalar,
-                          wavelengths=self.wavelengths)
+            kwargs = dict(
+                image_path=image_path,
+                image_ext=self.image_ext,
+                metadata_ext=self.metadata_ext,
+                channels=self.channels,
+                scalar=self.scalar,
+                wavelengths=self.wavelengths,
+            )
             if self.metadata_path is not None:
-                kwargs['metadata_path'] = self.metadata_path
+                kwargs["metadata_path"] = self.metadata_path
             self.listed_hyperstack.append(HyperspectralImage(**kwargs))
         return self
 
@@ -140,6 +136,7 @@ class MergedHyperspectralImage(BaseModel):
         first = self[0]
         val = getattr(first, attr)
         if callable(val):
+
             def delegated(*args, **kwargs):
                 results = []
                 for hs in self.listed_hyperstack:
@@ -166,5 +163,3 @@ class MergedHyperspectralImage(BaseModel):
 
     def __iter__(self):
         return iter(self.listed_hyperstack)
-
-
