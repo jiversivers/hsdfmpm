52e2378dade473c1b21469663edd2ffc7dfc5b46
jiversivers <jdivers@uark.edu>
2025-05-16 10:23:06 -0500

    removed (covered implicitly in af tests)

--- tests/hsdfmpm/mpm/test_utils_mpm.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestPrairieViewImage(unittest.TestCase):
-    def test_date(self):
-        self.assertEqual(True, False)  # add assertion here
-
-if __name__ == '__main__':
-    unittest.main()
