c700b4305479e8b086fc8b367c9f3a5b4f2faf94
jiversivers <jdivers@uark.edu>
2025-06-28 22:53:54 -0500

    autogen for docs and build

---
+++ conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'HSDFMPM'
+copyright = '2025, Jiversivers'
+author = 'Jiversivers'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+]
+
+autosummary_generate = True          # create stub pages on build
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "show-inheritance": True,
+}
+
+html_theme = 'furo'
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,20 @@
+.. HSDFM-MPM documentation master file, created by
+   sphinx-quickstart on Sat Jun 28 22:40:32 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+HSDFM-MPM documentation
+=======================
+
+.. autosummary::
+   :toctree: generated
+   :recursive:
+
+   hsdfmpm
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   README.md
+
